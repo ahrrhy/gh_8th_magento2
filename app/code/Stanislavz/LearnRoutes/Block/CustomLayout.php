c2e0c2af41983e604dc165dcb5dbdd7644fcff5e
ahrrhy <ahrrhy@gmail>
2018-10-31 01:35:19 +0200

    second task is not fully complited

--- app/code/Stanislavz/LearnRoutes/Block/CustomLayout.php
+++ app/code/Stanislavz/LearnRoutes/Block/CustomLayout.php
@@ -4,5 +4,13 @@ namespace Stanislavz\LearnRoutes\Block;
 
 class CustomLayout extends \Magento\Framework\View\Element\Template
 {
-    const CUSTOM_TEMPLATE = '';
+    const JSON_RESPONSE = 'homework/jsonresponse/index';
+
+    /**
+     * @return string
+     */
+    public function getLinkToJson()
+    {
+        return $this->getUrl(self::JSON_RESPONSE);
+    }
 }
--- app/code/Stanislavz/LearnRoutes/Controller/JsonResponse/Index.php
+++ app/code/Stanislavz/LearnRoutes/Controller/JsonResponse/Index.php
@@ -6,6 +6,9 @@ use Magento\Framework\Controller\ResultFactory;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     */
     public function execute()
     {
         /** var \Magento\Framework\Result\Json $controllerResult */
