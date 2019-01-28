178773a9d1a1c1ddfd17f7a016756c5cb38ece28
ahrrhy <ahrrhy@gmail>
2019-01-28 10:15:46 +0200

    hw-13 for review

--- app/code/Stanislavz/AskQuestion/Observer/Catalog/Block/Product/View.php
+++ app/code/Stanislavz/AskQuestion/Observer/Catalog/Block/Product/View.php
@@ -12,6 +12,9 @@ use Magento\Framework\Registry;
  */
 class View implements ObserverInterface
 {
+    /**
+     * Layout handle name we need to add
+     */
     public const LAYOUT_HANDLE = 'ask_question_tab';
     /**
      * @var Registry
@@ -34,10 +37,11 @@ class View implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        $event = $observer->getEvent();
-        $actionName = $event->getData('full_action_name');
+        $actionName = $observer->getEvent()->getData('full_action_name');
+        /** @var \Magento\Catalog\Model\Product $product */
         $product = $this->registry->registry('current_product');
-        $layout = $event->getData('layout');
+        /** @var \Magento\Framework\View\Layout $layout */
+        $layout = $observer->getEvent()->getData('layout');
 
         if ($product && $actionName === 'catalog_product_view' && $product->getAllowQuestion()) {
             $layout->getUpdate()->addHandle(static::LAYOUT_HANDLE);
