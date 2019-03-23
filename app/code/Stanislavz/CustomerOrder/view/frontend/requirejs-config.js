c233139002818f45e7a6fd53ee1843cb04a7c667
ahrrhy <ahrrhy@gmail>
2019-03-23 18:30:42 +0200

    hw-18: custom checkout step added

--- app/code/Stanislavz/CustomerOrder/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Checkout/js/action/set-shipping-information': {
-                'Stanislavz_CustomerOrder/js/action/set-shipping-information': true
-            }
-        }
-    }
-};
